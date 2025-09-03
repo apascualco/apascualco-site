7285c409ca868d1e9b65cf9500c83e9593cc839a
apascualco <apascualco@gmail.com>
2025-09-03 11:13:36 +0200

    fix: base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,7 @@
 
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://apascualco.github.io',
-  base: 'apascualco-site',
+  base: '/apascualco-site',
 });
