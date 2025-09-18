603b8bc648b90b0f7a1318299ad719044498f321
apascualco <apascualco@gmail.com>
2025-09-18 21:35:34 +0200

    first post: disonancia cognitiva

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@
 
 import { defineConfig } from 'astro/config';
 
+import mdx from '@astrojs/mdx';
+
 export default defineConfig({
   site: 'https://apascualco.github.io',
   base: '/apascualco-site',
+  integrations: [mdx()],
 });
