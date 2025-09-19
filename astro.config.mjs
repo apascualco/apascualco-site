bcd2e299febd119cf28e7d2a58555c9d44ec6b63
apascualco <apascualco@gmail.com>
2025-09-19 10:16:28 +0200

    Change astro configuration site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
 export default defineConfig({
-  site: 'https://apascualco.github.io',
-  base: '/apascualco-site',
+  site: 'https://www.apascualco.com',
+  base: '/',
   integrations: [mdx()],
 });
