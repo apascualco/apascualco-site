d6ece521f91ec4c88f2c6115ae6fa6c2af99313f
apascualco <apascualco@gmail.com>
2026-01-01 16:55:55 +0100

    Add ESLint tooling configuration

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
+
+export default [
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  {
+    ignores: ['dist/**', 'node_modules/**', '.astro/**'],
+  },
+  {
+    files: ['**/*.{js,ts,astro}'],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      'no-console': 'warn',
+    },
+  },
+];
