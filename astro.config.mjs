c80141915913a550f4598e15b11af01d5aee3329
apascualco <apascualco@gmail.com>
2026-01-01 17:49:20 +0100

    Enable sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 
 import { defineConfig } from 'astro/config';
-
 import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://www.apascualco.com',
   base: '/',
-  integrations: [mdx()],
+  integrations: [mdx(), sitemap()],
 });
